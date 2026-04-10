3472ff4362e881fed675d1e6675a45ea51549c25
hansol0118 <wall197944@naver.com>
2026-04-10 17:46:29 +0900

    init

---
+++ back/DevC/src/main/java/com/back/devc/DevCApplication.java
@@ -0,0 +1,13 @@
+package com.back.devc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevCApplication.class, args);
+    }
+
+}
