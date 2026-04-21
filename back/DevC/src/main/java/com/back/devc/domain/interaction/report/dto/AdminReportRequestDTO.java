7e4b67a6d846c10864dbd7da362805222677da01
hansol0118 <wall197944@naver.com>
2026-04-21 12:39:45 +0900

    refator:#162-AdminReportService targetType String ->  enum 변환

--- back/DevC/src/main/java/com/back/devc/domain/interaction/report/dto/AdminReportRequestDTO.java
+++ back/DevC/src/main/java/com/back/devc/domain/interaction/report/dto/AdminReportRequestDTO.java
@@ -1,10 +1,11 @@
 package com.back.devc.domain.interaction.report.dto;
 
 import com.back.devc.domain.interaction.report.entity.SanctionType;
+import com.back.devc.domain.interaction.report.entity.TargetType;
 
 public record AdminReportRequestDTO(
         Long reportId,
-        String targetType,
+        TargetType targetType,
         String adminNote,
         SanctionType sanctionType,
         Integer suspensionDays
